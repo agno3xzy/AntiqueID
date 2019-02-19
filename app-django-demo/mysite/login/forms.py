c278c34bffa0cb6f081d74f5e5baba4c91347a53
ybxybx <948868314@qq.com>
2019-02-20 07:14:50 +0800

    signup SQL

--- app-django-demo/mysite/login/forms.py
+++ app-django-demo/mysite/login/forms.py
@@ -3,3 +3,10 @@ from django import forms
 class UserForm(forms.Form):
     username = forms.CharField(label="用户名", max_length=128, widget=forms.EmailInput(attrs={'class': 'form-control' ,'aria-describedby':"emailHelp"}))
     password = forms.CharField(label="密码", max_length=256, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+
+class SignupForm(forms.Form):
+    user_id = forms.IntegerField(primary_key=True, auto_created=True, unique=True)
+    username = forms.CharField(label="用户名", max_length=128, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password1 = forms.CharField(label="密码", max_length=256, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2 = forms.CharField(label="确认密码", max_length=256, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(label="邮箱地址", widget=forms.EmailInput(attrs={'class': 'form-control'}))
--- app-django-demo/mysite/login/views.py
+++ app-django-demo/mysite/login/views.py
@@ -33,6 +33,42 @@ def signin(request):
 
 
 def signup(request):
+    if request.session.get('is_login', None):
+        # 登录状态不允许注册
+        return redirect("/index/")
+    if request.method == "POST":
+        signup_form = forms.SignupForm(request.POST)
+        message = "请检查填写的内容！"
+        if signup_form.is_valid():  # 获取数据
+            username = signup_form.cleaned_data['username']
+            password1 = signup_form.cleaned_data['password1']
+            password2 = signup_form.cleaned_data['password2']
+            email = signup_form.cleaned_data['email']
+            userid = request.POST.get('userid')
+
+            if password1 != password2:  # 判断两次密码是否相同
+                message = "两次输入的密码不同！"
+                return render(request, 'login/signup.html', locals())
+            else:
+                same_name_user = models.User.objects.filter(name=username)
+                if same_name_user:  # 用户名唯一
+                    message = '用户已经存在，请重新选择用户名！'
+                    return render(request, 'login/signup.html', locals())
+                same_email_user = models.User.objects.filter(email=email)
+                if same_email_user:  # 邮箱地址唯一
+                    message = '该邮箱地址已被注册，请使用别的邮箱！'
+                    return render(request, 'login/signup.html', locals())
+
+                # 当一切都OK的情况下，创建新用户
+
+                new_user = models.User()
+                new_user.name = username
+                new_user.password = password1
+                new_user.email = email
+                new_user.id = userid
+                new_user.save()
+                return redirect('/login/')  # 自动跳转到登录页面
+    signup_form = forms.SignupForm()
     return render(request, 'login/signup.html')
 
 def massagepage(request):
